5baca7aa661a6968e1148580bb5c0f8b7c558813
sherrinfordh23 <thanhantrinh2015@gmail.com>
2023-02-24 16:10:33 -0500

    Added Post and Category classes

---
+++ app/src/main/java/model/Post.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Post {
+
+    private String postId;
+    private String createdBy;
+    private Category category;
+    private String description;
+    private Location location;
+    private LocalDate timeFrame;
+    private double price;
+
+    public Post(String createdBy, Category category, String description, Location location, LocalDate timeFrame) {
+        this.postId = UUID.randomUUID().toString();
+        this.createdBy = createdBy;
+        this.category = category;
+        this.description = description;
+        this.location = location;
+        this.timeFrame = timeFrame;
+    }
+
+    public Post() {
+        this.postId = UUID.randomUUID().toString();
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public LocalDate getTimeFrame() {
+        return timeFrame;
+    }
+
+    public void setTimeFrame(LocalDate timeFrame) {
+        this.timeFrame = timeFrame;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "postId='" + postId + '\'' +
+                ", createdBy='" + createdBy + '\'' +
+                ", category=" + category +
+                ", description='" + description + '\'' +
+                ", location=" + location +
+                ", timeFrame=" + timeFrame +
+                ", price=" + price +
+                '}';
+    }
+}
