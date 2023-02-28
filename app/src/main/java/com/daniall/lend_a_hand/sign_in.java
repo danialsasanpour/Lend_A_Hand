013f1b291e24c8959c84c79b540c76f74ba72923
Danial Sasanpour <100496502+danialsasanpour@users.noreply.github.com>
2023-02-28 09:37:51 -0500

    Revert "Implemented Sign Up + Sign In"

--- app/src/main/java/com/daniall/lend_a_hand/sign_in.java
+++ app/src/main/java/com/daniall/lend_a_hand/sign_in.java
@@ -1,106 +1,14 @@
 package com.daniall.lend_a_hand;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import model.User;
-
-public class sign_in extends AppCompatActivity implements View.OnClickListener, ValueEventListener {
-
-    EditText edUsername, edPassword;
-    Button btnSignUp, btnSignIn;
-
-    FirebaseDatabase root = FirebaseDatabase.getInstance();
-    DatabaseReference users = root.getReference("Users");
-
-    String username;
+public class sign_in extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
-        initialize();
-    }
-
-    private void initialize()
-    {
-        edUsername = findViewById(R.id.editTextUsername);
-        username = (String) getIntent().getExtras().getSerializable("Username");
-        if (username != null) {
-            edUsername.setText(username);
-        }
-        edPassword = findViewById(R.id.editTextPassword);
-
-        btnSignUp = findViewById(R.id.btnSignUp);
-        btnSignUp.setOnClickListener(this);
-
-        btnSignIn = findViewById(R.id.btnSignIn);
-        btnSignIn.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnSignUp:
-                signUp();
-                break;
-            case R.id.btnSignIn:
-                signIn();
-                break;
-        }
-    }
-
-    private void signIn()
-    {
-        if(edUsername.getText().toString().equals("") ||
-                edPassword.getText().toString().equals("")
-        )
-            Toast.makeText(this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
-        else
-            users.child(edUsername.getText().toString()).addListenerForSingleValueEvent(this);
-    }
-
-    private void signUp() {
-        Intent i = new Intent(this, sign_up.class);
-        startActivity(i);
-    }
-
-    @Override
-    public void onDataChange(@NonNull DataSnapshot snapshot) {
-        if(snapshot.exists())
-        {
-            User userFound = snapshot.getValue(User.class);
-            if (userFound.getPassword().equals(edPassword.getText().toString()))
-            {
-                Intent i = new Intent(this, Home.class);
-                i.putExtra("currentUser", userFound);
-                startActivity(i);
-            }
-            else
-                Toast.makeText(this, "Username not found or password is invalid", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            Toast.makeText(this, "Username not found or password is invalid", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onCancelled(@NonNull DatabaseError error) {
-
     }
 }
--- app/src/main/java/com/daniall/lend_a_hand/sign_up.java
+++ app/src/main/java/com/daniall/lend_a_hand/sign_up.java
@@ -1,30 +1,17 @@
 package com.daniall.lend_a_hand;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+public class sign_up extends AppCompatActivity implements View.OnClickListener {
 
-import model.User;
-
-public class sign_up extends AppCompatActivity implements View.OnClickListener, ValueEventListener {
-
-    EditText edName, edEmail, edUsername, edPassword, edConfirmPassword, edPhoneNumber;
+    EditText edFirstName, edLastName, edEmail, edUsername, edPassword, edConfirmPassword;
     Button btnSignUp;
-
-    FirebaseDatabase root = FirebaseDatabase.getInstance();
-    DatabaseReference users = root.getReference("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +23,14 @@ public class sign_up extends AppCompatActivity implements View.OnClickListener, 
 
     private void initialize() {
 
-        edName = findViewById(R.id.editTextName);
+        edFirstName = findViewById(R.id.editTextFirstName);
+        edLastName = findViewById(R.id.editTextLastName);
         edEmail = findViewById(R.id.editTextEmail);
-        edPhoneNumber = findViewById(R.id.editTextPhoneNumber);
         edUsername = findViewById(R.id.editTextUsername);
         edPassword = findViewById(R.id.editTextPassword);
         edConfirmPassword = findViewById(R.id.editTextTextPasswordConfirm);
-
         btnSignUp = findViewById(R.id.btnSignUp);
+
         btnSignUp.setOnClickListener(this);
     }
 
@@ -59,7 +46,8 @@ public class sign_up extends AppCompatActivity implements View.OnClickListener, 
     private void createUser()
     {
         if(edEmail.getText().toString().equals("") ||
-                edName.getText().toString().equals("") ||
+                edFirstName.getText().toString().equals("") ||
+                edLastName.getText().toString().equals("") ||
                 edUsername.getText().toString().equals("") ||
                 edPassword.getText().toString().equals("") ||
                 edConfirmPassword.getText().toString().equals("")
@@ -67,57 +55,7 @@ public class sign_up extends AppCompatActivity implements View.OnClickListener, 
             Toast.makeText(this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
         else{
             String username = edUsername.getText().toString();
-            users.child(username).addListenerForSingleValueEvent(this);
+            // Users.child(username).addListenerForSingleValueEvent(this);
         }
     }
-
-    @Override
-    public void onDataChange(@NonNull DataSnapshot snapshot) {
-        if(!snapshot.exists())
-        {
-            try {
-                User newUser = new User();
-                newUser.setName(edName.getText().toString());
-                newUser.setEmail(edEmail.getText().toString());
-                newUser.setPhoneNumber(edPhoneNumber.getText().toString());
-                newUser.setUsername(edUsername.getText().toString());
-
-                if (edPassword.getText().toString().equals(edConfirmPassword.getText().toString()))
-                    newUser.setPassword(edPassword.getText().toString());
-                else
-                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-
-                users.child(edUsername.getText().toString()).setValue(newUser);
-                Toast.makeText(this,"Your account has been created successfully!", Toast.LENGTH_SHORT).show();
-
-                Intent i = new Intent(this, sign_in.class);
-                i.putExtra("Username", edUsername.getText().toString());
-                startActivity(i);
-
-            } catch (Exception e) {
-                Toast.makeText(this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(this, "The email or username "
-                    + edUsername.getText().toString()
-                    + " already exists", Toast.LENGTH_SHORT).show();
-
-            clearWidgets();
-        }
-    }
-
-    @Override
-    public void onCancelled(@NonNull DatabaseError error) {
-
-    }
-
-    public void clearWidgets(){
-        edEmail.setText(null);
-        edPhoneNumber.setText(null);
-        edUsername.setText(null);
-        edPassword.setText(null);
-        edConfirmPassword.setText(null);
-        edName.requestFocus();
-    }
-
 }
