39657f6b6518f837772a2f4599dd491d0dc2e6d4
danialsasanpour <danial.sasanpour2080@gmail.com>
2023-03-22 00:58:06 -0400

    test changes

---
+++ app/src/main/java/com/daniall/lend_a_hand/controllers/post_description.java
@@ -0,0 +1,14 @@
+package com.daniall.lend_a_hand.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class post_description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_description);
+    }
+}
