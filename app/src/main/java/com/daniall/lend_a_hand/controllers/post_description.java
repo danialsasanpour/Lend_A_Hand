c8738b1173e5bafe88b6ae572bc51828e116b85e
danialsasanpour <danial.sasanpour2080@gmail.com>
2023-03-22 01:11:23 -0400

    feat(ui/xml): activity_post_description & activity_making_post.xml
    I did this nothing but love

---
+++ app/src/main/java/com/daniall/lend_a_hand/controllers/post_description.java
@@ -0,0 +1,14 @@
+package com.daniall.lend_a_hand.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.daniall.lend_a_hand.R;
+import android.os.Bundle;
+
+public class post_description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_description);
+    }
+}
