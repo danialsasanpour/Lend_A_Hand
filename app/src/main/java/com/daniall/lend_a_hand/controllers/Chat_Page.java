2d4331dc3441d3b4ecb644c0008a80c06e8eacd4
danialsasanpour <danial.sasanpour2080@gmail.com>
2023-04-02 20:53:53 -0400

    editing error

--- app/src/main/java/com/daniall/lend_a_hand/controllers/Chat_Page.java
+++ app/src/main/java/com/daniall/lend_a_hand/controllers/Chat_Page.java
@@ -3,6 +3,7 @@ package com.daniall.lend_a_hand.controllers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.daniall.lend_a_hand.R;
 
 public class Chat_Page extends AppCompatActivity {
 
