0bcf49be76260309ea6ead7135f41464ebb2ec4b
danialsasanpour <danial.sasanpour2080@gmail.com>
2023-04-02 20:52:13 -0400

    adding chat page

---
+++ app/src/main/java/com/daniall/lend_a_hand/controllers/Chat_Page.java
@@ -0,0 +1,14 @@
+package com.daniall.lend_a_hand.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chat_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+    }
+}
