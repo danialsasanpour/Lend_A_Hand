ea142c8cc2c092277c9f7511ffd851d3ba2328dc
danialsasanpour <danial.sasanpour2080@gmail.com>
2023-04-07 00:04:07 -0400

    adding account details page to the project

---
+++ app/src/main/java/com/daniall/lend_a_hand/controllers/Account_Details.java
@@ -0,0 +1,15 @@
+package com.daniall.lend_a_hand.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.daniall.lend_a_hand.R;
+
+import android.os.Bundle;
+
+public class Account_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_details);
+    }
+}
