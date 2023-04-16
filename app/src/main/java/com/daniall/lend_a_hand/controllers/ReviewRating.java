0a3f397d5665e4a5415749f329215a42db994d6c
abu-ullah <abu.ullah03@gmail.com>
2023-04-16 16:53:28 -0400

    Commented out things in reviewRatings java file to avoid merging conflicts

--- app/src/main/java/com/daniall/lend_a_hand/controllers/ReviewRating.java
+++ app/src/main/java/com/daniall/lend_a_hand/controllers/ReviewRating.java
@@ -15,65 +15,65 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import model.User;
 
-public class ReviewRating extends AppCompatActivity implements View.OnClickListener {
+public class ReviewRating extends AppCompatActivity {
 
-    User byUser;
-    User toUser;
-
-    EditText edReview;
-    Button btnPost;
-
-    ImageButton imageButtonHome, imageButtonMessage, imageButtonAccount;
-
-    FirebaseDatabase root = FirebaseDatabase.getInstance();
-    DatabaseReference users = root.getReference("Users");
-    DatabaseReference reviewRatings = root.getReference("ReviewRatings");
+//    User byUser;
+//    User toUser;
+//
+//    EditText edReview;
+//    Button btnPost;
+//
+//    ImageButton imageButtonHome, imageButtonMessage, imageButtonAccount;
+//
+//    FirebaseDatabase root = FirebaseDatabase.getInstance();
+//    DatabaseReference users = root.getReference("Users");
+//    DatabaseReference reviewRatings = root.getReference("ReviewRatings");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_rating);
 
-        initialize();
+        //initialize();
     }
 
-    public void initialize() {
-        byUser = (User) getIntent().getExtras().getSerializable("byUser");
-        toUser = (User) getIntent().getExtras().getSerializable("toUser");
+//    public void initialize() {
+//        byUser = (User) getIntent().getExtras().getSerializable("byUser");
+//        toUser = (User) getIntent().getExtras().getSerializable("toUser");
+//
+//        edReview = findViewById(R.id.edReview);
+//
+//        btnPost = findViewById(R.id.btnPost);
+//        btnPost.setOnClickListener(this);
+//
+//        imageButtonHome = findViewById(R.id.imageButtonHome);
+//        imageButtonMessage = findViewById(R.id.imageButtonMessage);
+//        imageButtonAccount = findViewById(R.id.imageButtonAccount);
+//
+//        imageButtonHome.setOnClickListener(this);
+//        imageButtonMessage.setOnClickListener(this);
+//        imageButtonAccount.setOnClickListener(this);
+//    }
 
-        edReview = findViewById(R.id.edReview);
+//    @Override
+//    public void onClick(View view)
+//    {
+//        switch (view.getId())
+//        {
+//            case R.id.btnPost:
+//                postReview(view);
+//                break;
+//        }
+//    }
 
-        btnPost = findViewById(R.id.btnPost);
-        btnPost.setOnClickListener(this);
-
-        //imageButtonHome = findViewById(R.id.imageButtonHome);
-        //imageButtonMessage = findViewById(R.id.imageButtonMessage);
-        //imageButtonAccount = findViewById(R.id.imageButtonAccount);
-
-        //imageButtonHome.setOnClickListener(this);
-        //imageButtonMessage.setOnClickListener(this);
-        //imageButtonAccount.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view)
-    {
-        switch (view.getId())
-        {
-            case R.id.btnPost:
-                postReview(view);
-                break;
-        }
-    }
-
-    private void postReview(View view)
-    {
-        if (edReview.getText().toString() == "")
-        {
-            Toast.makeText(this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
+//    private void postReview(View view)
+//    {
+//        if (edReview.getText().toString() == "")
+//        {
+//            Toast.makeText(this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
 //        ReviewRating review = new ReviewRating(
 //                byUser.getUsername(),
 //                toUser.getUsername(),
@@ -81,6 +81,6 @@ public class ReviewRating extends AppCompatActivity implements View.OnClickListe
 //                // float rating
 //        );
 //        reviewRatings.child(review.getReviewId()).setValue(review);
-
-    }
+//
+//    }
 }
